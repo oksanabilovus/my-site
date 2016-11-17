54d3e29cd7068042d2d02cbefcd543a0a671bb62
Oksana Bilovus <o.top@ukr.net>
2016-11-17 12:02:02 +0000

    h

--- js/index.js
+++ js/index.js
@@ -6,3 +6,30 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+$("iframe").addClass('embed-responsive-item');
+$("iframe").wrap('<div class="embed-responsive embed-responsive-16by9"/>');
+
+
+
+function animate(elem){
+    var effect = elem.data("effect");
+    if(!effect || elem.hasClass(effect)) return false;
+    elem.addClass(effect);
+    setTimeout( function(){
+        elem.removeClass(effect);
+    }, 1000);
+}
+ 
+$(".animated").click(function() {
+    animate($(this));
+});
+
+ wow = new WOW( {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+ })
+                    wow.init();
